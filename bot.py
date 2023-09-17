bdf32c04d4666810063ef09239dd0ed743fcdd63
Netanel Friedman <netanel.friedman6@gmail.com>
2023-09-17 20:59:41 +0300

    Create a main function, so I can `return` on error and short-circuit the program

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import re
-from data_fetch import get_daily_stock_data
+from typing import Optional
+from data_fetch import get_daily_stock_data, StockData, StockDataError
 
-def extract_stock_symbol(message: str) -> str:
+
+def extract_stock_symbol(message: str) -> Optional[str]:
     # Use regular expressions to extract the stock symbol from the message
     match = re.match(r'!stock\s+(\w+)', message)
     if match:
@@ -9,25 +11,30 @@ def extract_stock_symbol(message: str) -> str:
     else:
         return None
 
-if __name__ == '__main__':
+
+def main():
     user_input: str = input('Enter your command: ')  # Get user input, e.g., "!stock NVDA"
 
-    stock_symbol: str = extract_stock_symbol(user_input)
+    stock_symbol: Optional[str] = extract_stock_symbol(user_input)
 
-    if stock_symbol:
-        stock_data: dict = get_daily_stock_data(stock_symbol)
-
-        if 'error' in stock_data:
-            print(f'Error: {stock_data["error"]}')
-        else:
-            print(f'Stock Data for {stock_data["symbol"]} on {stock_data["date"]}:')
-            print(f'Open: {stock_data["open"]}')
-            print(f'Close: {stock_data["close"]}')
-            print(f'Volume: {stock_data["volume"]}')
-            print(f'Daily Change: {stock_data["daily_change"]}')
-            print(f'Daily Change Percentage: {stock_data["daily_change_percentage"]}')
-            print(f'P/E Ratio: {stock_data["pe_ratio"]}')
-            print(f'Target Price: {stock_data["target_price"]}')
-            print(f'Bullish/Bearish Recommendations: {stock_data["bullish_bearish_recommendations"]}')
-    else:
+    if stock_symbol is None:
         print('Invalid command format. Please use the format "!stock <company>".')
+        return
+
+    try:
+        stock_data: StockData = get_daily_stock_data(stock_symbol)
+        print(f'Stock Data for {stock_data.symbol} on {stock_data.date}:')
+        print(f'Open: {stock_data.open}')
+        print(f'Close: {stock_data.close}')
+        print(f'Volume: {stock_data.volume}')
+        print(f'Daily Change: {stock_data.daily_change}')
+        print(f'Daily Change Percentage: {stock_data.daily_change_percentage}')
+        print(f'P/E Ratio: {stock_data.pe_ratio}')
+        print(f'Target Price: {stock_data.target_price}')
+        print(f'Bullish/Bearish Recommendations: {stock_data.bullish_bearish_recommendations}')
+    except StockDataError as e:
+        print("Error:", e)
+
+
+if __name__ == '__main__':
+    main()
