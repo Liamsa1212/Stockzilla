c51bcae1ad1e131757c8b5ba52bff01246423040
Liamsa1212 <Liamsa47@gmail.com>
2023-09-17 20:32:04 +0300

    add dataclass for bot

--- bot.py
+++ bot.py
@@ -1,5 +1,20 @@
 import re
 from data_fetch import get_daily_stock_data
+from config import ALPHA_VANTAGE_API_KEY
+from dataclasses import dataclass
+
+@dataclass
+class StockData:
+    symbol: str
+    date: str
+    open: str
+    close: str
+    volume: str
+    daily_change: str
+    daily_change_percentage: str
+    pe_ratio: str
+    target_price: str
+    bullish_bearish_recommendations: str
 
 def extract_stock_symbol(message: str) -> str:
     # Use regular expressions to extract the stock symbol from the message
@@ -15,19 +30,19 @@ if __name__ == '__main__':
     stock_symbol: str = extract_stock_symbol(user_input)
     
     if stock_symbol:
-        stock_data: dict = get_daily_stock_data(stock_symbol)
+        stock_data: StockData = get_daily_stock_data(stock_symbol)
         
-        if 'error' in stock_data:
-            print(f'Error: {stock_data["error"]}')
+        if 'error' in stock_data.__dict__:
+            print(f'Error: {stock_data.__dict__["error"]}')
         else:
-            print(f'Stock Data for {stock_data["symbol"]} on {stock_data["date"]}:')
-            print(f'Open: {stock_data["open"]}')
-            print(f'Close: {stock_data["close"]}')
-            print(f'Volume: {stock_data["volume"]}')
-            print(f'Daily Change: {stock_data["daily_change"]}')
-            print(f'Daily Change Percentage: {stock_data["daily_change_percentage"]}')
-            print(f'P/E Ratio: {stock_data["pe_ratio"]}')
-            print(f'Target Price: {stock_data["target_price"]}')
-            print(f'Bullish/Bearish Recommendations: {stock_data["bullish_bearish_recommendations"]}')
+            print(f'Stock Data for {stock_data.symbol} on {stock_data.date}:')
+            print(f'Open: {stock_data.open}')
+            print(f'Close: {stock_data.close}')
+            print(f'Volume: {stock_data.volume}')
+            print(f'Daily Change: {stock_data.daily_change}')
+            print(f'Daily Change Percentage: {stock_data.daily_change_percentage}')
+            print(f'P/E Ratio: {stock_data.pe_ratio}')
+            print(f'Target Price: {stock_data.target_price}')
+            print(f'Bullish/Bearish Recommendations: {stock_data.bullish_bearish_recommendations}')
     else:
         print('Invalid command format. Please use the format "!stock <company>".')
